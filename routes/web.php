51aee61261662ad286b4b215061a006b860d9529
Knight <francisco_1320114043@uptecamac.edu.mx>
2022-03-04 19:34:13 -0600

    Subiendo para poder probar

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\http\Controllers\ApiController;
+use App\Http\Controllers\ApiController;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Rutas para personajes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| Establece una ruta con la API de Marvel para mostrar personajes
 |
 */
 
