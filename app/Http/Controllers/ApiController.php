0a84c590ec7aea467ccf8dd44277ae32e410c91b
Knight <francisco_1320114043@uptecamac.edu.mx>
2022-03-05 00:03:34 -0600

    1.6

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -16,7 +16,7 @@ class ApiController extends Controller
         // -- Recorrer elementos
         $personajes = [];
 
-        foreach ($datos['data.results'] as $personaje) {
+        foreach ($datos['data:results'] as $personaje) {
             $personajes[] = [
                 'id' => $personaje['id'],
                 'nombre' => $personaje['name'],
