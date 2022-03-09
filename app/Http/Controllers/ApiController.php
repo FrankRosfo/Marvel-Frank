d2d5406cf8ffe98d31841074df0d470df8719965
Knight <francisco_1320114043@uptecamac.edu.mx>
2022-03-08 23:34:27 -0600

    comics

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
-    public function Personajes(){
+    public function personajes(){
         // -- consumir api Marvel
         $cliente = new \GuzzleHttp\Client();
         $response = $cliente->request('GET', 'https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=06ffa280d1bafc06d930b43d6d8dd14b&hash=afda8720864a69268e1e8bedd7a23b60');
@@ -52,7 +52,7 @@ class ApiController extends Controller
         return view('detalles',['personaje' => $personajeIndividual, 'personajes' => $personajes]);
     }
 
-    public function Comics(){
+    public function comics(){
 
         // -- consumir comics
         $cliente = new \GuzzleHttp\Client();    
