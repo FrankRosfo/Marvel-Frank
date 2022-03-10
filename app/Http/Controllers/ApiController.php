8911ef8a2c7ef3ab9d18a94b8b0133a1b07a2891
Knight <francisco_1320114043@uptecamac.edu.mx>
2022-03-10 02:32:16 -0600

    Urls 1.0

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -71,9 +71,9 @@ class ApiController extends Controller
                 'thumbnail_path' => $comic['thumbnail']['path'],
                 'thumbnail_extension' => $comic['thumbnail']['extension'],
                 'series_name' => $comic['series']['name'],
-                'series_resourdeURI' => $comic['series']['resourceURI']
+                'series_resourceURI' => $comic['series']['resourceURI']
             ];
-            foreach ($datos_comics['urls'] as $link) {
+            foreach ($datos_comics['data']['results']['urls'] as $link) {
                 $links[] = [
                     'type' => $link['type'],
                     'url' => $link['url']
