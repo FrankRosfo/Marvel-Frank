a24013e092950ef4feb2d06993a431907dd2cad1
Knight <francisco_1320114043@uptecamac.edu.mx>
2022-03-11 05:16:47 -0600

    Paginas

--- resources/views/template/fondo.blade.php
+++ resources/views/template/fondo.blade.php
@@ -12,13 +12,13 @@
     <title>Marvel/Frank</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
 
     <!-- Additional CSS Files -->
-    <link rel="stylesheet" href="assets/css/fontawesome.css">
-    <link rel="stylesheet" href="assets/css/templatemo-stand-blog.css">
-    <link rel="stylesheet" href="assets/css/owl.css">
+    <link rel="stylesheet" href="/assets/css/fontawesome.css">
+    <link rel="stylesheet" href="/assets/css/templatemo-stand-blog.css">
+    <link rel="stylesheet" href="/assets/css/owl.css">
 <!--
 
 TemplateMo 551 Stand Blog
@@ -85,17 +85,17 @@ https://templatemo.com/tm-551-stand-blog
     </footer>
 
     <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="/vendor/jquery/jquery.min.js"></script>
+    <script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <!-- Additional Scripts -->
-    <script src="assets/js/custom.js"></script>
-    <script src="assets/js/owl.js"></script>
-    <script src="assets/js/slick.js"></script>
-    <script src="assets/js/isotope.js"></script>
-    <script src="assets/js/accordions.js"></script>
+    <script src="/assets/js/custom.js"></script>
+    <script src="/assets/js/owl.js"></script>
+    <script src="/assets/js/slick.js"></script>
+    <script src="/assets/js/isotope.js"></script>
+    <script src="/assets/js/accordions.js"></script>
 
-    <script language = "text/Javascript"> 
+    <script language = "/text/Javascript"> 
     cleared[0] = cleared[1] = cleared[2] = 0; //set a cleared flag for each field
     function clearField(t){                   //declaring the array outside of the
     if(! cleared[t.id]){                      // function makes it static and global
