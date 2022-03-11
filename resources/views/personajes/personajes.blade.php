8ba6e1d975ee60c578fa81fdc39ed40ca56caf20
Knight <francisco_1320114043@uptecamac.edu.mx>
2022-03-11 06:20:41 -0600

    Flechas

--- resources/views/personajes/personajes.blade.php
+++ resources/views/personajes/personajes.blade.php
@@ -70,14 +70,12 @@
             @endforeach
             <div class="col-lg-12">
               <ul class="page-numbers">
-                @if($page>1){
+                @if($page > 1)
                   <li><a href="{{route('personajes',$page-1)}}"><i class="fa fa-angle-double-left"></i></a></li>
-                }
                 @endif
                 <li class="active"><a href="{{route('personajes',$page)}}">{{ $page }}</a></li>
-                @if($page<150){
+                @if($page < 150)
                   <li><a href="{{route('personajes',$page+1)}}"><i class="fa fa-angle-double-right"></i></a></li>
-                }
                 @endif
               </ul>
             </div>
