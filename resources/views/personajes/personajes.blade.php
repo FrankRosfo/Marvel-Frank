a94c95d884a8c56e65006d20768dc5000546c093
Knight <francisco_1320114043@uptecamac.edu.mx>
2022-03-11 08:01:14 -0600

    Active

--- resources/views/personajes/personajes.blade.php
+++ resources/views/personajes/personajes.blade.php
@@ -73,7 +73,7 @@
                 @if($page>1)
                 <li><a href="{{route('personajes',$page-1)}}"><i class="fa fa-angle-double-left"></i></a></li>
                 @endif
-                  <li><a href="{{route('personajes',$page)}}">{{ $page }}</a></li>
+                  <li class="active"><a href="{{route('personajes',$page)}}">{{ $page }}</a></li>
                 @if($page<150)
                 <li><a href="{{route('personajes',$page+1)}}"><i class="fa fa-angle-double-right"></i></a></li>
                 @endif
